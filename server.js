57aab84356135d5c864b7ecd663b7cd62d81f1e1
adamhenson <adamhenson1979@gmail.com>
2014-12-01 12:47:15 -0500

    to be on the safe side.

--- server.js
+++ server.js
@@ -27,4 +27,4 @@ process.on('uncaughtException', function (err) {
   console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
   console.error(err.stack)
   process.exit(1)
-})
+});
