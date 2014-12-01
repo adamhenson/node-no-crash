4d6ab75828f648c2bd2400cbb2a11e425ae1b86a
adamhenson <adamhenson1979@gmail.com>
2014-12-01 15:55:50 -0500

    use cluster the heroku way.

--- server.js
+++ server.js
@@ -1,30 +1,22 @@
 var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 var start = require('./start');
-
-var workers = process.env.WORKERS || require('os').cpus().length;
 
 if (cluster.isMaster) {
 
-  console.log('start cluster with %s workers', workers);
+  console.log('start cluster with %s workers', numCPUs);
 
-  for (var i = 0; i < workers; ++i) {
-    var worker = cluster.fork().process;
-    console.log('worker %s started.', worker.pid);
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
 
-  cluster.on('exit', function(worker) {
-    console.log('worker %s died. restart...', worker.process.pid);
-    cluster.fork();
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
   });
-
+  
 } else {
-
+  
   start();
 
 }
-
-process.on('uncaughtException', function (err) {
-  console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
-  console.error(err.stack)
-  process.exit(1)
-});
