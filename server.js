a2b7b720312628977ecdaecba32a1caa3f2dac27
adamhenson <adamhenson1979@gmail.com>
2014-12-02 14:51:36 -0500

    use forever to crash and restart automatically.

--- server.js
+++ server.js
@@ -1,28 +1,13 @@
-var cluster = require('cluster');
-var numCPUs = require('os').cpus().length;
-var start = require('./start');
+var forever = require('forever-monitor');
 
-if (cluster.isMaster) {
-
-  console.log('start cluster with %s workers', numCPUs);
-
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died... restarting.');
-    cluster.fork();
-  });
-
-} else {
-  
-  start();
-
-}
-
-process.on('uncaughtException', function (err) {
-  console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
-  process.exit(1)
+var child = new (forever.Monitor)('start.js', {
+  max: 100,
+  silent: false,
+  args: []
 });
+
+child.on('exit', function () {
+  console.log('exited after 100 restarts');
+});
+
+child.start();
--- start.js
+++ start.js
@@ -1,24 +1,20 @@
-module.exports = function() {
+// setup
 
-	// setup
+var env             = process.env.NODE_ENV || 'dev';
+var port            = process.env.PORT || 8080;
+var express         = require('express');
+var app             = express();
 
-	var env             = process.env.NODE_ENV || 'dev';
-	var port            = process.env.PORT || 8080;
-	var express         = require('express');
-	var app             = express();
+// configuration
 
-	// configuration
+app.use(express.static(__dirname + "/public"));
 
-	app.use(express.static(__dirname + "/public"));
+// routes
 
-	// routes
+require('./app/routes.js')(app);
 
-	require('./app/routes.js')(app);
+// launch
 
-	// launch
-
-	app.listen(port, function () {
-	  console.log('%s listening at port %s', app.name, port);
-	});
-
-};
+app.listen(port, function () {
+  console.log('%s listening at port %s', app.name, port);
+});
